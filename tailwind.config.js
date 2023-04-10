4e62d90a9b8a2470fe15ab2db476ec101d617166
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2023-04-10 14:47:14 +0330

    Added Tailwind required tool

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      body: ["Bakh"],
+      display: ["Bakh"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
